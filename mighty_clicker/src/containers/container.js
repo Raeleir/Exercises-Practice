e2a37b6849ed55ae624a5bb50febb6f252acf133
Joseph Carroll <sagamyr@gmail.com>
2017-07-10 15:14:38 -0600

    clicker

--- mighty_clicker/src/containers/container.js
+++ mighty_clicker/src/containers/container.js
@@ -7,31 +7,46 @@ class Container extends React.Component {
     constructor() {
         super();
         this.state = {
-            red: 0,
-            blue: 0
+            red: 50,
+            blue: 50
         }
         autoBind(this);
     }
     click(color) {
-        if(color === "blue"){
-            this.setState({
-                ...this.state,
-                red: this.state.red - 1,
-                blue: this.state.blue + 1
-            })
-        } else {
-            this.setState({
-                ...this.state,
-                red: this.state.red + 1,
-                blue: this.state.blue - 1
-            })
+        this.setState(prevState =>{
+            if(prevState.red >= 100 || prevState.blue >= 100){
+                return {
+                    red: 50,
+                    blue: 50
+                }
+            }
+            if(color === "blue"){
+                return {
+                    ...this.state,
+                    red: this.state.red - 1,
+                    blue: this.state.blue + 1
+                }
+            } else {
+                return {
+                    ...this.state,
+                    red: this.state.red + 1,
+                    blue: this.state.blue - 1
+                }
         }
+        });
     }
+    
     render() {
         return ( 
             <div>
                 <Button handleClick={this.click} color="red" counter={this.state.red}/>
                 <Button handleClick={this.click} color="blue" counter={this.state.blue}/>
+                <div className="bars">
+                <div className="red" style={{width: this.state.red + "%"}}>
+                </div>
+                <div className="blue" style={{width: this.state.blue + "%"}}>
+                </div>
+                </div>
             </div> 
         )
     }
