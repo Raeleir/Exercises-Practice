e8bc0ce97ffe5d62815670f1a783c8862e62844f
Joseph Carroll <sagamyr@gmail.com>
2017-07-26 14:58:39 -0600

    grid game

--- node-review/grid.js
+++ node-review/grid.js
@@ -1,41 +1,17 @@
 let rs = require("readline-sync");
-
-let grid = [];
-
-let print = (arr) => {
-    for(i=0;i<arr.length;i++){
-      console.log(...arr[i]);
-    }
-    console.log("\n");
-}
-
-let genGrid = (num) =>{
-    for(i=0;i<num;i++){
-        grid.push([]);
-        for(j=0;j<num;j++){
-            grid[i].push("0");
-        }
-    }
-    console.log(`Here is your ${num} x ${num} grid: \n`);
-    print(grid);
-}
-
-let update = (char, x, y) => {
-    grid[y - 1].splice(x - 1, 1, char.toUpperCase());
-    console.log(`Here is your updated grid! \n`);
-    print(grid);
-}
-
+let funcs = require("./grid-actions.js");
 
 let size = Number(rs.question("What size grid would you like? \n"));
-genGrid(size);
+funcs.genGrid(size);
 
 let isActive = true;
 while(isActive) {
-    let char = rs.question("What character would you like to enter into the grid? \n");
+    let char = rs.question("What character would you like to enter into the grid? \n")
+    
     let x = Number(rs.question(`Which column would you like your "${char}" to go in? \n`));
     let y = Number(rs.question(`Which row would you like your "${char}" to go in? \n`));
-    update(char, x, y);
-    let cont = rs.keyInYN("Would you like to add another character? \n");
-    !cont ? isActive = false: isActive = true;
+    
+    funcs.update(char, x, y);
+    
+    !rs.keyInYN("Would you like to add another character? \n") ? isActive = false: isActive = true;
 }
